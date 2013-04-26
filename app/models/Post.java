cca05f66343ffc05f16446289b98062915342049
Ian Murphy <iano200@gmail.com>
2013-04-26 10:36:23 +0100

    Starting Off

--- app/models/Post.java
+++ app/models/Post.java
@@ -1,5 +1,28 @@
 package models;
 
-public class Post {
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Lob;
+import javax.persistence.OneToMany;
+
+import play.db.jpa.Model;
+
+@Entity
+public class Post extends Model {
+	public String title;
+	
+	@Lob
+	public String content;
+	
+	@OneToMany (cascade = CascadeType.ALL)
+	public List<Comment> comments;
+	
+	public Post(String title, String content) {
+		this.title = title;
+		this.content = content;
+		this.comments = new ArrayList<Comment>();
+	}
 }
--- app/models/User.java
+++ app/models/User.java
@@ -1,5 +1,48 @@
 package models;
 
-public class User {
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import play.db.jpa.Blob;
+import play.db.jpa.Model;
+
+@Entity
+public class User extends Model {
+	public String   firstName;
+	public String   lastName;
+	public int      age;
+	public String   email;
+	public String   password;
+	public Blob     profilePicture;
+	public Blob     avatar;
+	
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	public List<Blog> blogs;
+	
+	public User(String firstName, String lastName, int age, String email, String password) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;		
+		this.email = email;
+		this.password = password;
+		blogs = new ArrayList<Blog>();
+	}
+	
+	public void addBlog(Blog blog) {
+		blogs.add(blog);
+	}
+	
+	public boolean checkPassword(String password) {
+		return this.password.equals(password);
+	}
+	
+	public String toString() {
+		return firstName;
+	}
 
 }
